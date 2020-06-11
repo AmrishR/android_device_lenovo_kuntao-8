6e685b7796825fb4e86f3d0f25a1599fe667ffae
AmrishR <ramrish7799@gmail.com>
2020-06-11 11:11:15 +0000

    Havoc bringup

--- havoc_kuntao.mk
+++ havoc_kuntao.mk
@@ -26,13 +26,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Aex stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/kuntao/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_kuntao
+PRODUCT_NAME := havoc_kuntao
 PRODUCT_DEVICE := kuntao
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo P2a42
